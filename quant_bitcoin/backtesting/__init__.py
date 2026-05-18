d5994044f3143b64516f2bb87c82dc764a27d92d
김성호 <chzhqk98@naver.com>
2026-05-18 15:45:30 +0630

    Clarify FVG pattern backtest CLI

--- quant_bitcoin/backtesting/__init__.py
+++ quant_bitcoin/backtesting/__init__.py
@@ -7,19 +7,29 @@ from quant_bitcoin.backtesting.basic import (
     BacktestTrade,
 )
 from quant_bitcoin.backtesting.pattern_strategy import (
+    DEFAULT_PATTERN,
+    SUPPORTED_PATTERNS,
+    PatternName,
     PatternStrategyBacktestConfig,
     PatternStrategyBacktestResult,
     PatternStrategyBacktestTrade,
     run_pattern_strategy_backtest,
+    strategy_name_for_patterns,
+    validate_pattern_selection,
 )
 
 __all__ = [
+    "DEFAULT_PATTERN",
+    "SUPPORTED_PATTERNS",
     "BasicBacktester",
     "BacktestResult",
     "BacktestSummary",
     "BacktestTrade",
+    "PatternName",
     "PatternStrategyBacktestConfig",
     "PatternStrategyBacktestResult",
     "PatternStrategyBacktestTrade",
     "run_pattern_strategy_backtest",
+    "strategy_name_for_patterns",
+    "validate_pattern_selection",
 ]
